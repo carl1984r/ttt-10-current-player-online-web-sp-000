853d5e55945f8ca566b250e3071732bb77dc0f6e
Bry Cox <carl.cc@gmail.com>
2019-01-02 22:35:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,14 @@
 
 
 def turn_count(board)
-
 board.count do |v|
   v == "X" || v == "O"
 end
 end
 
-def current_player
+def current_player(board)
+  if turn_count(board) % 2 == 0
+     return "X"
+  else
+     return "O"
 end
